dcf2eac7fed1c30b21899ae58d13cc8becf84939
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-06-24 23:11:56 -0500

    learned paradigm of jsx

--- notebook.jsx
+++ notebook.jsx
@@ -1,2 +1,12 @@
 //JSX is a javascript extension. It's the language of react
 const h1 = <h1>Hello world</h1>;
+
+//JSX is essentially javascript objects with HTML as the value
+const p1 = <p id="large">foo</p>;
+const p2 = <p id="small">bar</p>;
+
+const myDiv = (
+  <div>
+    <h1>Hello world</h1>
+  </div>
+);
