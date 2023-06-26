25b3557be29adae9744dc489d94ec13878d4a5d3
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-06-25 23:33:43 -0500

    learned something about react root

--- notebook.jsx
+++ notebook.jsx
@@ -10,3 +10,13 @@ const myDiv = (
     <h1>Hello world</h1>
   </div>
 );
+
+//this is doing something with the root object. i have no idea what
+//and somehow it renders hello world like html but doesn't replace html
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("container");
+
+const root = createRoot(container);
+root.render(<h1>Hello world</h1>);
